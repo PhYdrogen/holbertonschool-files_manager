951ffccd98ae7a21695f48e8704f84f42e5c4281
Gabriel <fossecruor@gmail.com>
2025-01-10 11:35:29 +0100

    init repo

---
+++ utils/redis.mjs
@@ -0,0 +1,31 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('err', (err) => console.log(err));
+    // this.client.on('connect', () => console.log('is now connected'));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve) => {
+      this.client.get(key, (_err, reply) => resolve(reply));
+    });
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    setTimeout(() => this.client.del(key), duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
