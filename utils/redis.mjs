033e4af9d2a3b6b21d62c2becbce9954693b2cf8
Gabriel <fossecruor@gmail.com>
2025-01-11 14:23:29 +0100

    promise everywhere

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { createClient } from 'redis';
 
 class RedisClient {
@@ -13,18 +12,26 @@ class RedisClient {
   }
 
   async get(key) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
+      if (key === '') reject(Error('No key'));
+      if (typeof key !== 'string') reject(Error('Key must be string'));
       this.client.get(key, (_err, reply) => resolve(reply));
     });
   }
 
   async set(key, value, duration) {
-    this.client.set(key, value);
-    setTimeout(() => this.client.del(key), duration);
+    return new Promise((resolve, reject) => {
+      if (key === '') reject(Error('No key'));
+      if (typeof key !== 'string') reject(Error('Key must be string'));
+      resolve(this.client.set(key, value));
+      setTimeout(() => this.client.del(key), duration);
+    });
   }
 
   async del(key) {
-    this.client.del(key);
+    return new Promise((resolve) => {
+      resolve(this.client.del(key));
+    });
   }
 }
 
