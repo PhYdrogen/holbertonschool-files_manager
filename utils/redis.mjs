f6132160e5f8707195d832eafd887a7db1888ae4
Gabriel <fossecruor@gmail.com>
2025-01-11 15:17:27 +0100

    update redis

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,10 +1,11 @@
-import { createClient } from 'redis';
+import { promisify } from 'util';
+
+const redis = require('redis');
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.client.on('err', (err) => console.log(err));
-    this.client.on('connect', () => console.log('is now connected'));
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.log(error.message));
   }
 
   isAlive() {
@@ -12,31 +13,20 @@ class RedisClient {
   }
 
   async get(key) {
-    return new Promise((resolve, reject) => {
-      if (key === '' || key == null) reject(Error('No key'));
-      if (typeof key !== 'string') reject(Error('Key must be string'));
-      this.client.get(key, (_err, reply) => resolve(reply));
-    });
+    const getval = await promisify(this.client.get).bind(this.client);
+    const val = await getval(key);
+    return val;
   }
 
-  async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      if (key === '' || key == null) reject(Error('No key'));
-      if (value == null) reject(Error('value is required'));
-      if (duration <= 0 || duration == null) reject(Error('duration must be greater than 0'));
-      if (typeof key !== 'string') reject(Error('Key must be string'));
-      resolve(this.client.set(key, value));
-      setTimeout(() => this.client.del(key), duration * 1000);
-    });
+  async set(key, val, duration) {
+    this.client.set(key, val);
+    this.client.expire(key, duration);
   }
 
   async del(key) {
-    return new Promise((resolve, reject) => {
-      if (key === '' || key == null) reject(Error('No key'));
-      resolve(this.client.del(key));
-    });
+    this.client.del(key);
   }
 }
 
 const redisClient = new RedisClient();
-export default redisClient;
+module.exports = redisClient;
