c3946156ec9a7c205f3965840f4670e17325e73d
Gabriel <fossecruor@gmail.com>
2025-01-11 16:12:20 +0100

    debugger is back

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./routes/index').default;
+const debug = require('./debug').default;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ app.use(router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  debug.send();
 });
 
 export default app;
