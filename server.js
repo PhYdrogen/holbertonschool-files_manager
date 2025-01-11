d95c931546591d9559fc697c0bb3ffbabc41f07b
Gabriel <fossecruor@gmail.com>
2025-01-11 15:55:59 +0100

    correct

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./routes/index').default;
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(router);
